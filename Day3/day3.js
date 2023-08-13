b67f31f229428f90a82e3377f02a84bf4078c271
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-08-13 15:17:48 +0700

    new commit

--- Day3/day3.js
+++ Day3/day3.js
@@ -6,10 +6,36 @@ fetch("https://jsonplaceholder.typicode.com/photos")
     }
   })
   .then(function (data) {
+    let newArray = [];
     for (let i = 0; i < 10; i++) {
-      console.log(data[i]);
+      newArray.push(data[i]);
+      //   console.log(data[i].id);
     }
+
+    // call function here
+    renderItem(newArray);
   })
   .catch(function (err) {
     console.log(err);
   });
+
+// function render item
+function renderItem(arrayPost) {
+  let container = document.querySelector(".container");
+
+  for (let i = 0; i < arrayPost.length; i++) {
+    let createNewDiv = document.createElement("div");
+    createNewDiv.className = "item";
+    createNewDiv.innerHTML = `
+
+    <p id="idItem">${arrayPost[i].id}</p>
+    <h1 class="title">
+        ${arrayPost[i].title}
+    </h1>
+
+    <img src="${arrayPost[i].url}" alt="" />
+
+`;
+    container.appendChild(createNewDiv);
+  }
+}
